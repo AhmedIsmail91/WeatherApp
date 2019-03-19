28b5a7acbb8c2e189dd9cdbda73462306f105dce
AHMED <https://TechiSher@bitbucket.org/TechiSher/test.git>
2019-03-19 18:00:58 +0200

    Adding Buttom Incrementer

--- app/src/main/java/com/waether/app/features/randomizer/randomizerActivity.kt
+++ app/src/main/java/com/waether/app/features/randomizer/randomizerActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+//import com.example.usecases.incrementNumber
 import com.waether.app.R
 import kotlinx.android.synthetic.main.activity_randomizer.*
 
@@ -17,7 +18,8 @@ class RandomizerActivity : FragmentActivity()
         val viewModel = ViewModelProviders.of(this).get(RandomizerViewModel::class.java)
         viewModel.numberLiveData.observe(this,
             Observer {random_number_textView.text = it.toString()})
-        /// neww
+
+        increment_button.setOnClickListener({viewModel.incrementNumber()})
     }
 }
 
--- app/src/main/java/com/waether/app/features/randomizer/randomizerViewModel.kt
+++ app/src/main/java/com/waether/app/features/randomizer/randomizerViewModel.kt
@@ -3,22 +3,27 @@ package com.waether.app.Feature.randomizer
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.example.usecases.Ticker
-import com.example.usecases.randomNumberGenerator
+import com.example.usecases.numberIncrementer
+//import com.example.usecases.randomNumberGenerator
 
 private const val DEFAULT_VALUE=0
 
 class RandomizerViewModel : ViewModel()
 {
-   private val ticker = initializeTicker()
+  // private val ticker = initializeTicker()
     val numberLiveData = MutableLiveData<Int>()
 
     init {
         numberLiveData.value=DEFAULT_VALUE
-        ticker.start()
+       // ticker.start()
     }
 
-    private fun initializeTicker() = Ticker {
-        numberLiveData.postValue(randomNumberGenerator().toInt())
+//    private fun initializeTicker() = Ticker {
+//        numberLiveData.postValue(randomNumberGenerator().toInt())
+//    }
+
+    fun  incrementNumber(){
+        numberIncrementer(numberLiveData)
     }
 
     override fun onCleared() {
